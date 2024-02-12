d1b7073b2b6105c5006d23f7ff69ac923b3aed00
JoelFaldin <joelfaldin@gmail.com>
2024-02-11 23:05:23 -0300

    feat: added project

--- index.js
+++ index.js
@@ -14,19 +14,49 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
+
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", (req, res) => {
+  const date = new Date(req.params.date);
+
+  if (isNaN(+req.params.date) && date == 'Invalid Date') {
+    res.json({ error: "Invalid Date" });
+  } else if (+req.params.date.length === 13) {
+    const utc = new Date(+req.params.date).toUTCString();
+
+    res.json({
+      unix: +req.params.date,
+      utc: utc
+    })
+  } else {
+    const unix = new Date(req.params.date).getTime();
+    const utc = new Date(req.params.date).toUTCString();
+
+    res.json({ unix, utc });
+  }
 });
 
+// 
+app.get("/api", (req, res) => {
+  const unixDate = new Date().getTime();
+  const utcDate = new Date().toUTCString();
+  
+  const dateObject = {
+    unix: unixDate,
+    utc: utcDate
+  }
 
+  res.json(dateObject);
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
+  console.log(`http://localhost:${listener.address().port}/`)
 });
